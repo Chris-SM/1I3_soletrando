f95b15d7d931bc1af4724471f7b9979deadef578
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-03-15 22:35:42 -0300

    Adicionado um programa em C# para soletrar

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿string tex = "a";
+Console.WriteLine("----------------- Soletrar -----------------");
+Console.WriteLine("Digite a palavra que quer soletrar: ");
+tex = Console.ReadLine()!.ToUpper();
+Console.WriteLine(
+ String.Join("-",tex.ToCharArray()));
